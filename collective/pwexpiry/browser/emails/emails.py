18abd5c882019e39a48927a91625fcafea943b4c
Joshua LaPlace <jlaplace@enfoldsystems.com>
2015-03-16 14:08:33 -0500

    Alter notification email to support days <= 0.

--- collective/pwexpiry/browser/emails/emails.py
+++ collective/pwexpiry/browser/emails/emails.py
@@ -1,9 +1,14 @@
 from Products.Five import BrowserView
-from Products.CMFCore.utils import getToolByName
+
 
 class NotificationEmail(BrowserView):
 
     def __call__(self, **kwargs):
         """
         """
+        self.days = kwargs['days']
         return self.index(**kwargs)
+    
+    @property
+    def has_expired(self):
+        return self.days <= 0
