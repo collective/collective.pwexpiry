6c66d9bace9729192f3b0239304c455763b27677
Franco Pellegrini <frapell@gmail.com>
2015-03-17 17:26:45 -0300

    When comparing dates, make sure both are timezone aware

--- collective/pwexpiry/utils.py
+++ collective/pwexpiry/utils.py
@@ -7,6 +7,9 @@ from plone import api
 
 from collective.pwexpiry.config import _
 
+import pytz
+
+
 def send_notification_email(user, days_to_expire, email_view='notification_email'):
     """
     """
@@ -27,5 +30,11 @@ def days_since_event(event_date, current_date):
     Returns the number of days difference
     between two given dates
     """
+    # make both dates timezone aware
+    if not event_date.tzinfo:
+        event_date = pytz.utc.localize(event_date)
+    if not current_date.tzinfo:
+        current_date = pytz.utc.localize(current_date)
+
     difference = current_date - event_date
     return difference.days
