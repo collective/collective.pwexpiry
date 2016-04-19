b8b18ab31a547f813e78bc88bc735e0fe76f228b
Rene Jochum <rene@jochums.at>
2016-04-19 12:48:06 +0200

    Fix an encoding problem with usernames in notify_and_expire
    Signed-off-by: Rene Jochum <rene@jochums.at>

--- collective/pwexpiry/utils.py
+++ collective/pwexpiry/utils.py
@@ -2,6 +2,7 @@
 import pytz
 from collective.pwexpiry.config import _
 from plone import api
+from Products.CMFPlone.utils import safe_unicode
 from zope.i18n import translate
 
 
@@ -16,33 +17,33 @@ def send_notification_email(user, days_to_expire):
     recipient = user.getProperty('email')
 
     msg_mapping = {
-        u"username": user.getProperty('fullname'),
-        u"days": days_to_expire,
+        'username': safe_unicode(user.getProperty('fullname')),
+        'days': days_to_expire,
     }
     if days_to_expire > 0:
         msg = translate(
             _('email_text',
-              default="""Hello ${username},
+              default=u"""Hello ${username},
 
 There are ${days} days left before your password expires!
 
 Please ensure to reset your password before it's expired.
 """,
               mapping=msg_mapping,
-            ),
+              ),
             target_language=language
         )
     else:
         msg = translate(
             _('email_text_expired',
-              default="""Hello ${username},
+              default=u"""Hello ${username},
 
 Your password has expired.
 
 Please ensure to reset your password before it's expired.
 """,
               mapping=msg_mapping,
-            ),
+              ),
             target_language=language
         )
 
@@ -72,4 +73,3 @@ def days_since_event(event_date, current_date):
 
     difference = current_date - event_date
     return difference.days
-
