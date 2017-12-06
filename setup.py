2f4b6e979fef213de6815657a1f7c9dea6c289c4
Harald Friessnegger <harald@webmeisterei.com>
2017-12-06 11:30:57 +0100

    Back to development: 0.11.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.11.3'
+version = '0.11.4.dev0'
 
 setup(name='collective.pwexpiry',
       version=version,
