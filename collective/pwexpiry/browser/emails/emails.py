b6433d23d10bb79ce97ea5602ab29eafc8d5f1d1
Franco Pellegrini <frapell@gmail.com>
2018-05-24 17:03:49 -0300

    Use SERVER_URL and SERVER_NAME for including additional info in emails

--- collective/pwexpiry/browser/emails/emails.py
+++ collective/pwexpiry/browser/emails/emails.py
@@ -15,7 +15,8 @@ class NotificationEmail(BrowserView):
         )
 
         body = email_template(**{
-            'username': safe_unicode(user.getProperty('fullname')),
+            'username': user.getUserId(),
+            'fullname': safe_unicode(user.getProperty('fullname')),
             'days': days_to_expire,
             'language': language_code,
         })
@@ -24,17 +25,43 @@ class NotificationEmail(BrowserView):
 
         msg_mapping = {
             'username': kwargs['username'],
+            'fullname': kwargs['fullname'],
             'days': kwargs['days'],
         }
+
+        if self.request.get('SERVER_URL', 'http://foo') == 'http://foo':
+            server_url = ""
+        else:
+            server_url = self.request.get('SERVER_URL')
+
+        if server_url.endswith('/'):
+            server_url = server_url[:-1]
+
+        msg_mapping['server_url'] = server_url
+
+        if self.request.get('SERVER_NAME', 'foo') == 'foo':
+            server_name = ""
+        else:
+            server_name = self.request.get('SERVER_NAME')
+
+        msg_mapping['server_name'] = server_name
+
         if kwargs['days'] > 0:
             msg = translate(
                 _('email_text',
-                  default=u"""Hello ${username},
+                  default=u"""Hello ${fullname},
 
 There are ${days} days left before your password expires!
 
-Please ensure to reset your password before it's expired.
-""",
+Please ensure to reset your password before it's expired.""",
+                  mapping=msg_mapping,
+                  ),
+                target_language=language
+            )
+
+            msg += translate(
+                _('change_password_email_text',
+                  default=u"""\n\nIn order to change your password, please visit ${server_url}/@@change-password""",
                   mapping=msg_mapping,
                   ),
                 target_language=language
@@ -42,12 +69,28 @@ Please ensure to reset your password before it's expired.
         else:
             msg = translate(
                 _('email_text_expired',
-                  default=u"""Hello ${username},
+                  default=u"""Hello ${fullname},
 
 Your password has expired.
 
-Please ensure to reset your password before it's expired.
-""",
+Please ensure to reset your password before it's expired.""",
+                  mapping=msg_mapping,
+                  ),
+                target_language=language
+            )
+
+            msg += translate(
+                _('reset_password_email_text',
+                  default=u"""\n\nIn order to reset your password, please visit ${server_url}/mail_password_form?userid=${username}""",
+                  mapping=msg_mapping,
+                  ),
+                target_language=language
+            )
+
+        if msg_mapping['server_name'] != "":
+            msg += translate(
+                _('server_name_email_text',
+                  default=u"""\n\nThis email was sent from ${server_name}""",
                   mapping=msg_mapping,
                   ),
                 target_language=language
