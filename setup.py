f3dac6af81abe008ab395d39676831cd7c8219fd
Franco Pellegrini <frapell@gmail.com>
2018-05-30 16:03:57 -0400

    Preparing release 0.12.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.11.4.dev0'
+version = '0.12.0'
 
 setup(name='collective.pwexpiry',
       version=version,
