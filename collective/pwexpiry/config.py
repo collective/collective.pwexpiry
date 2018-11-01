2467a0bafc9924d225263d61c6e7e3d0089f389e
hvelarde <hector.velarde@gmail.com>
2018-11-01 12:29:11 -0300

    Add basic install/uninstall tests

--- collective/pwexpiry/config.py
+++ collective/pwexpiry/config.py
@@ -1,6 +1,12 @@
+# -*- coding: utf-8 -*-
+from plone import api
 from zope.i18nmessageid import MessageFactory
 
 _ = MessageFactory("collective.pwexpiry")
 
+PROJECTNAME = 'collective.pwexpiry'
+
 # Ideally, this should be language dependent, but we'll do US default for now
 DATETIME_FORMATSTRING = "%m/%d/%Y %H:%M"
+
+IS_PLONE_5 = api.env.plone_version().startswith('5')
